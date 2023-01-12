1dc13f41f3ca3527d9f6d353b93d37054ec9bcd2
Michael Gulson <mgulson@optoro.com>
2023-01-11 20:06:31 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+default_languages = ['javascript', 'java', 'python','c','c++', 'ruby',
+ 'go', 'c#', 'typescript', 'sql', 'swift', 'rust', 'kotlin']
+
+default_languages.each do | language|
+  Language.create(name: language)
+end
+puts 'created default languages'
